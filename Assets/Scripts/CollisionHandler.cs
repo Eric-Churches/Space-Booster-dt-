fe031a418058dbe606573e1f9beddd267da038d5
Eric Churches <59458107+Eric-Churches@users.noreply.github.com>
2022-07-03 16:11:27 +1200

    change the names around

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,24 +1,41 @@
 using UnityEngine;
 using Debug = UnityEngine.Debug;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
     void OnCollisionEnter(Collision other)
     {
-        switch(other.gameObject.tag)
+        switch (other.gameObject.tag)
         {
             case "Friendly":
-            Debug.Log("You are safe in this area");
-            break;
+                Debug.Log("You are safe in this area");
+                break;
             case "Finish":
-                Debug.Log("Congrats you have finish Yeah!!");
+                LoadNextLevel();
                 break;
             case "Fuel":
                 Debug.Log("I need some gas brother");
                 break;
             default:
-                
+                ReloadLevel();
                 break;
+        }
+
+        void LoadNextLevel()
+        {
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(currentSceneIndex + 1);
+
+
+            void ReloadLevel()
+            {
+                int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+                SceneManager.LoadScene(currentSceneIndex);
+            }
+
         }
     }
 }
+
+    
